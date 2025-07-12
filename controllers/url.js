d8398da42488b744b9965d393e24959a5043a31a
Peeyushnaik <peeyushnaik@gmail.com>
2025-07-12 21:10:45 +0530

    Added redirect error handling and comments for clarity

--- controllers/url.js
+++ controllers/url.js
@@ -2,12 +2,12 @@ const { nanoid } = require('nanoid'); //For shortid generation
 const URL = require("../models/url");
 
 async function handleGenerateNewShortURL(req, res) {
-  const body = req.body;
+  const body = req.body; //Can only be used once a middleware is initialized
   if (!body.url) return res.status(400).json({ error: 'URL is required' });
-
+//Body.url can only be written when post request has url in its body , if body has something else written like "content" then you'll have to write body.content
   const shortID = nanoid(8); // Generates an 8-character ID
 
-  await URL.create({
+  await URL.create({//MongoDB's function used to create data
     shortID: shortID,
     redirectURL: body.url,
     visitHistory: [],
@@ -18,10 +18,10 @@ async function handleGenerateNewShortURL(req, res) {
 
 async function handleGetAnalytics(req,res){
   const shortId = req.params.shortId;
-  const result = await URL.findOne({shortID:shortId});
+  const result = await URL.findOne({shortID:shortId}); //FindOne MongoDB's function which checks wheather shortID:shortId is present in db or not
   return res.json({
-    totalClicks:result.visitHistory.length,
-    analytics:result.visitHistory,
+    totalClicks:result.visitHistory.length, //Size of array basically how many times that shortID was visited before
+    analytics:result.visitHistory, //Data that when visited timestamps
   });
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {connectMongoDB} = require("./connection");
-const urlRoute = require('./routes/url');//I could have named it router only instead of urlRoute but it is just fine
+const urlRoute = require('./routes/url'); //I could have named it router only instead of urlRoute but it is just fine
 const URL = require("./models/url");
 const app = express();
 
@@ -10,17 +10,17 @@ connectMongoDB("mongodb://127.0.0.1:27017/short-url")
 .then(()=> console.log("MongoDB connected")
 );
 
-app.use(express.json())//Middleware
+app.use(express.json()) //Middleware
+//Beneficial as it parses the data (converts the data from one form to another) here to json format
 
 app.use("/url",urlRoute);
 app.get('/:shortId', async (req,res) => {
     const shortId = req.params.shortId;
-    const entry = await URL.findOneAndUpdate(
+    const entry = await URL.findOneAndUpdate( //FindOneAndUpdate MongoDB's function will check wheather shortID attribute mein shortId is present or not
+        {shortID:shortId},
+
         {
-            shortID:shortId,
-        },
-        {
-            $push:{
+            $push:{ //$push will update the timestamp to latest visited in visitHistory array
                 visitHistory:{
                     timestamp: Date.now(),
                 }
@@ -28,7 +28,13 @@ app.get('/:shortId', async (req,res) => {
         }
     );
 
-    res.redirect(entry.redirectURL);
+    if(!entry){ //Condition to check if the shortid does actually exist or not in the MongoDB
+        return res.status(404).send("Short URL not found")
+    }
+
+    res.redirect(entry.redirectURL); //Here we are able to use redirectURL as it was defined in our schema
+    //Entry is a variable which is full of JavaScript objects like redirectURL, visitHistory, shortId
+    //redirect will redirect us to our original url that we gave
 });
 
 app.listen(PORT,() => console.log("Server Started!"));
